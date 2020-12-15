82849fb3b90be40353ae76972d8b55c942434f11
Erik Körner <koerner@informatik.uni-leipzig.de>
2020-12-15 14:12:35 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name="transformer-discord-notifier",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     description="A Discord Notifier to send progress updates, params and results to a Discord channel.",
     long_description="%s\n%s"
