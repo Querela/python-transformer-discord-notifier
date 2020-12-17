23e85332d3e2f1152fe09e9fae8c8df4a8485b30
Erik Körner <koerner@informatik.uni-leipzig.de>
2020-12-17 13:30:23 +0100

    Bump version: 0.3.0 → 0.3.1

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = "Transformer Discord Notifier"
 year = "2020"
 author = "Erik Körner"
 copyright = "{0}, {1}".format(year, author)
-version = release = "0.3.0"
+version = release = "0.3.1"
 
 pygments_style = "trac"
 templates_path = ["."]
